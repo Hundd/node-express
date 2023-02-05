2f1f3f6f2a67383dc4887ab792c3d7fc07551cf5
Andrii Polchaninov <Andrii_Polchaninov@epam.com>
2023-02-05 16:39:14 +0200

    add health check

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 80;
 
 app.get("/", (req, res) => {
+  console.log("hello..");
   res.send("Hello World!");
+});
+
+app.get("/health-check", (req, res) => {
+  console.log("/health-check");
+  res.json({ message: "Server up and running" });
 });
 
 app.listen(port, () => {
